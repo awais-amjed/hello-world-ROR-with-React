514229ad9001d3c786187ba6e3920185b6301f73
Awais Amjed <awaisamjed171@gmail.com>
2022-07-04 19:51:43 +0500

    Setup basic hello app with React

---
+++ app/javascript/react/src/components/Hello.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const Hello = () => (<div>Hello, Rails 7!</div>)
+
+export default Hello
---
+++ app/javascript/react/src/index.js
@@ -0,0 +1,4 @@
+import { define } from 'remount'
+import Hello from "./components/Hello"
+
+define({ 'react-app': Hello })
